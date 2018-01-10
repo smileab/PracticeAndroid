b6ef196773c91de05c616fb99a57db00557901b5
smileab <1977606751@qq.com>
2018-01-10 12:14:17 +0800

    no message

--- app/src/main/java/com/smileab/mobile/practice/ui/MainActivity.java
+++ app/src/main/java/com/smileab/mobile/practice/ui/MainActivity.java
@@ -32,6 +32,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        // 
+
         ButterKnife.bind(this);
 
 
